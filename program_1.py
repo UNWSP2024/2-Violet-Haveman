ab602eb790dc56019c734ab999d06700a83f8daa
Violet-Haveman <vkhaveman@students.unwsp.edu>
2025-01-24 16:07:02 -0600

    Week 2 Programs
    Completed Week 2 Programs

--- program_1.py
+++ program_1.py
@@ -4,3 +4,11 @@ def personal_information():
 
 # Line which calls the above function.
 personal_information()
+name = "Violet Haveman"
+address = "123456 Hamilton Rd. S. NY 503267"
+number = "6516738278"
+major = "Art and Computer Science"
+print (name)
+print (address)
+print (number)
+print (major)
